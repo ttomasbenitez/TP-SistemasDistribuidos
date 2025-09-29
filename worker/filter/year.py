f94ab43015f89a57c1c1f4d28e43c7e3adb3b256
carolinamauro <carolina@ip-192-168-0-26.ec2.internal>
2025-09-29 15:04:30 -0300

    initial worker interface and filter by year worker

---
+++ worker/filter/year.py
@@ -0,0 +1,34 @@
+from worker.worker import Worker 
+from Middleware.middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
+
+class FilterYear(Worker):
+    
+    def __init__(self, in_queue: MessageMiddlewareQueue, out_queue: MessageMiddlewareQueue, out_exchange: MessageMiddlewareExchange, years_set):
+        super().__init__(in_queue)
+        self.out_queue = out_queue
+        self.out_exchange = out_exchange
+        self.years = years_set
+        
+    def __on_message__(self, message):
+        try:
+            items = message.process_meesage()
+            new_chunk = b''
+            for item in items:
+                year = item.get_year()
+                if year in self.years:
+                    new_chunk += item.serialize()
+            if new_chunk:
+                new_message = message.update_content(new_chunk)
+                serialized = new_message.serialize()
+                self.out_queue.send(serialized)
+                self.out_exchange.send(serialized)
+        except Exception as e:
+            print(f"Error al procesar el mensaje: {type(e).__name__}: {e}")
+            
+    def close(self):
+        try:
+            self.in_middleware.close()
+            self.out_queue.close()
+            self.out_exchange.close()
+        except Exception as e:
+            print(f"Error al cerrar: {type(e).__name__}: {e}")
