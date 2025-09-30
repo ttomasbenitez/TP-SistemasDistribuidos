da8b0b31de1f282fd17188ba27c70abe93246fad
Tomás <tomivenitez.14@gmail.com>
2025-09-29 22:41:08 -0300

    Add main per file.

---
+++ utils/utils.py
@@ -0,0 +1,14 @@
+import logging
+
+def initialize_log(logging_level):
+    """
+    Python custom logging initialization
+
+    Current timestamp is added to be able to identify in docker
+    compose logs the date when the log has arrived
+    """
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging_level,
+        datefmt='%Y-%m-%d %H:%M:%S',
+    )
--- worker/filter/year.py
+++ worker/filter/year.py
@@ -2,6 +2,8 @@ from worker import Worker
 from Middleware.middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
 import logging
 from pkg.message.message import Message
+from utils.utils import initialize_log
+import os
 
 class FilterYearNode(Worker):
     
@@ -37,3 +39,37 @@ class FilterYearNode(Worker):
             self.out_exchange.close()
         except Exception as e:
             print(f"Error al cerrar: {type(e).__name__}: {e}")
+
+def initialize_config():
+    """ Parse env variables to find program config params
+
+    Function that search and parse program configuration parameters in the
+    program environment variables first and the in a config file. 
+    If at least one of the config parameters is not found a KeyError exception 
+    is thrown. If a parameter could not be parsed, a ValueError is thrown. 
+    If parsing succeeded, the function returns a dict with config parameters
+    """
+
+    config_params = {}
+    
+    config_params["rabbitmq_host"] = os.getenv('RABBITMQ_HOST')
+    config_params["input_queue"] = os.getenv('INPUT_QUEUE_1')
+    config_params["logging_level"] = os.getenv('LOG_LEVEL', 'INFO')
+
+    if config_params["rabbitmq_host"] is None or config_params["input_queue"] is None:
+        raise ValueError("Expected value not found. Aborting filter.")
+    
+    return config_params
+
+def main():
+    config_params = initialize_config()
+
+    initialize_log(config_params["logging_level"])
+
+    input_queue = MessageMiddlewareQueue(config_params["rabbitmq_host"], config_params["input_queue"])
+    
+    filter = FilterYearNode(input_queue, None, None, {2024, 2025})
+    filter.start()
+
+if __name__ == "__main__":
+    main()
