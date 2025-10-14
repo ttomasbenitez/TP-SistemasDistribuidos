140b79705610a411a64e7b8fedb5b14e1ee4cd56
EFrascarelli <efrascarelli@fi.uba.ar>
2025-10-14 01:13:38 -0300

    Se agregan funcionalidades de JoinerQ3

--- worker/joiner/q3_stores_joiner.py
+++ worker/joiner/q3_stores_joiner.py
@@ -21,23 +21,23 @@ class StoresJoiner(Worker):
         self.pending_transactions = list()
         self.processed_transactions = dict()  
         self.stores = {}
-        self.eof_count = 0
+        self.eofs_by_client = {}
 
     def __on_message__(self, msg):
         message = Message.deserialize(msg)
         logging.info(f"Received message | request_id: {message.request_id} | type: {message.type}")
 
         if message.type == MESSAGE_TYPE_EOF:
-            self.eof_count += 1
-            if self.eof_count < EXPECTED_EOFS:
-                logging.info(f"EOF recibido {self.eof_count}/{EXPECTED_EOFS}")
+            self.eofs_by_client[message.request_id] = self.eofs_by_client.get(message.request_id, 0) + 1
+            if self.eofs_by_client[message.request_id] < EXPECTED_EOFS:
+                logging.info(f"EOF recibido {self.eofs_by_client[message.request_id]}/{EXPECTED_EOFS}")
                 return
 
             # --- ÚLTIMO EOF: procesar y publicar usando out_mw (channel separado) ---
             try:
-                self._process_pending()                  # solo CPU/mem
-                self.send_joined_transactions(message)   # publica en out_mw (OUT channel)
-                self._send_eof(message)                  # publica EOF en out_mw
+                self._process_pending()                                                 # solo CPU/mem
+                self.send_joined_transactions_by_request(message, message.request_id)   # publica en out_mw (OUT channel)
+                self._send_eof(message)                                                 # publica EOF en out_mw
             except Exception as e:
                 # Si algo falla aquí, logueá el tipo exacto (p.ej. ChannelClosedByBroker)
                 logging.exception(f"Error publicando resultados finales: {type(e).__name__}: {e}")
@@ -52,33 +52,37 @@ class StoresJoiner(Worker):
 
         if message.type == MESSAGE_TYPE_STORES:
             for item in items:
-                self.stores[item.get_id()] = item.get_name()
+                self.stores[message.request_id] = {item.get_id(): item.get_name()}
+                # self.stores[item.get_id()] = item.get_name()
 
         elif message.type == MESSAGE_TYPE_QUERY_3_INTERMEDIATE_RESULT:
             for item in items:
-                store_name = self.stores.get(item.get_store())
+                store_name = self.stores.get(message.request_id, {}).get(item.get_store())
                 if store_name:
-                    key = (store_name, item.get_period())
+                    key = (message.request_id, store_name, item.get_period())
                     self.processed_transactions[key] = self.processed_transactions.get(key, 0.0) + item.get_tpv()
                 else:
-                    self.pending_transactions.append(item)
+                    item_to_append = (item, message.request_id)
+                    self.pending_transactions.append(item_to_append)
 
-    def send_joined_transactions(self, message):
+    def send_joined_transactions_by_request(self, message, request_id):
         total_chunk = ''
         for (key, total_tpv) in self.processed_transactions.items():
-            store_name, period = key
+            request_id, store_name, period = key
+            if request_id != request_id:
+                continue
             q3Result = Q3Result(period, store_name, total_tpv)
             total_chunk += q3Result.serialize()
         msg = Message(message.request_id, MESSAGE_TYPE_QUERY_3_RESULT, message.msg_num, total_chunk)
         self.out_queue.send(msg.serialize())
 
     def _process_pending(self):
-        for item in self.pending_transactions:
-            store_name = self.stores.get(item.get_store())
+        for item, request_id in self.pending_transactions:
+            store_name = self.stores.get(request_id, {}).get(item.get_store())
             if store_name:
-                key = (store_name, item.get_period())
+                key = (request_id, store_name, item.get_period())
                 self.processed_transactions[key] = self.processed_transactions.get(key, 0.0) + item.get_tpv()
-                self.pending_transactions.remove(item)
+                self.pending_transactions.remove((item, request_id))
 
     def _send_eof(self, message):
         self.out_queue.send(message.serialize())
@@ -92,8 +96,6 @@ class StoresJoiner(Worker):
         except Exception as e:
             logging.error(f"Error al cerrar: {type(e).__name__}: {e}")
 
-
-    
     def _request_stop_consuming_async(self):
         def _stop():
             try:
