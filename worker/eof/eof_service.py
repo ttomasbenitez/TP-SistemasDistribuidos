a21304dfd412cb00479a6632c036f6ccf8a636cf
carolinamauro <carolina@ip-192-168-0-142.ec2.internal>
2025-10-15 20:20:04 -0300

    wip: eof service modularizado

--- worker/eof/eof_service.py
+++ worker/eof/eof_service.py
@@ -1,17 +1,15 @@
 from worker.base import Worker 
-from Middleware.middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
+from Middleware.middleware import MessageMiddlewareQueue, MessageMiddleware
 import logging
 from pkg.message.message import Message
 from pkg.message.constants import MESSAGE_TYPE_EOF
-import os
-from utils.custom_logging import initialize_log, setup_process_logger
+from abc import ABC, abstractmethod
 
-
-class EofService(Worker):
+class EofService(Worker, ABC):
   
-    def __init__(self, expected_acks: int, eof_in_queque: MessageMiddlewareQueue, eof_out_exchange: MessageMiddlewareExchange):
+    def __init__(self, expected_acks: int, eof_in_queque: MessageMiddlewareQueue, eof_out_middleware: MessageMiddleware):
         super().__init__(eof_in_queque)
-        self.eof_out_exchange = eof_out_exchange
+        self.eof_out_middleware = eof_out_middleware
         self.expected_acks = expected_acks
         self.acks_by_client = dict()
       
@@ -23,7 +21,7 @@ class EofService(Worker):
                 self.acks_by_client[message.request_id] = self.acks_by_client.get(message.request_id, 0) + 1  
                 if self.acks_by_client[message.request_id] == self.expected_acks:
                     logging.info(f"Enviando final EOF del cliente {message.request_id}")
-                    self.eof_out_exchange.send(message.serialize(), str(message.type))
+                    self.send_message_to_output(message)
                     del self.acks_by_client[message.request_id]
         except Exception as e:
             logging.error(f"Error al procesar el mensaje: {type(e).__name__}: {e}")
@@ -33,71 +31,13 @@ class EofService(Worker):
             self.in_middleware.send(message)
         except Exception as e:
             print(f"Error al enviar el mensaje: {type(e).__name__}: {e}")
-              
-            
+    
+    @abstractmethod         
+    def send_message_to_output(self, message):
+        pass
+                
     def close(self):
         try:
             self.in_middleware.close()
         except Exception as e:
             print(f"Error al cerrar: {type(e).__name__}: {e}")
-
-
-def initialize_config():
-    """ Parse env variables to find program config params
-
-    Function that search and parse program configuration parameters in the
-    program environment variables first and the in a config file. 
-    If at least one of the config parameters is not found a KeyError exception 
-    is thrown. If a parameter could not be parsed, a ValueError is thrown. 
-    If parsing succeeded, the function returns a dict with config parameters
-    """
-    config_params = {
-        "rabbitmq_host": os.getenv('RABBITMQ_HOST'),
-        "input_queue": os.getenv('INPUT_QUEUE'),
-        "output_queue_1": os.getenv('OUTPUT_QUEUE_1'),
-        "output_queue_2": os.getenv('OUTPUT_QUEUE_2'),
-        "output_queue_3": os.getenv('OUTPUT_QUEUE_3', ''), # Ver cómo hacemos con este caso
-        "output_exchange_filter": os.getenv('EXCHANGE_NAME'),
-        "logging_level": os.getenv('LOG_LEVEL', 'INFO'),
-        "expected_acks": int(os.getenv('EXPECTED_ACKS')),
-    }
-
-    required_keys = [
-        "rabbitmq_host",
-        "input_queue",
-        "output_queue_1",
-        "output_queue_2",
-        "output_exchange_filter",
-    ]
-
-    missing_keys = [key for key in required_keys if config_params[key] is None]
-    if missing_keys:
-        raise ValueError(f"Expected value(s) not found for: {', '.join(missing_keys)}. Aborting filter.")
-    
-    return config_params
-
-def main():
-    config_params = initialize_config()
-
-    initialize_log(config_params["logging_level"])
-
-    eof_input_queue = MessageMiddlewareQueue(config_params["rabbitmq_host"], config_params["input_queue"])
-
-    if config_params["output_queue_3"] == '':
-        eof_output_exchange = MessageMiddlewareExchange(config_params["rabbitmq_host"], config_params["output_exchange_filter"], 
-                                        {config_params["output_queue_1"]: [str(MESSAGE_TYPE_EOF)], 
-                                        config_params["output_queue_2"]: [str(MESSAGE_TYPE_EOF)]})
-    else:
-        eof_output_exchange = MessageMiddlewareExchange(config_params["rabbitmq_host"], config_params["output_exchange_filter"], 
-                                            {config_params["output_queue_1"]: [str(MESSAGE_TYPE_EOF)], 
-                                            config_params["output_queue_2"]: [str(MESSAGE_TYPE_EOF)], 
-                                            config_params["output_queue_3"]: [str(MESSAGE_TYPE_EOF)]})
-
-    eof_service = EofService(
-        expected_acks=config_params["expected_acks"],
-        eof_in_queque=eof_input_queue,
-        eof_out_exchange=eof_output_exchange
-    )
-    eof_service.start()
-if __name__ == "__main__":
-    main()
---
+++ worker/eof/eof_service_time.py
@@ -0,0 +1,69 @@
+from worker.eof.eof_service import EofService
+import logging
+import os
+from utils.custom_logging import initialize_log
+from Middleware.middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
+from pkg.message.constants import MESSAGE_TYPE_EOF
+
+class EofServiceTime(EofService):
+    
+    def send_message_to_output(self, message):
+        try:
+            routing_key = str(message.type)
+            self.eof_out_middleware.send(message.serialize(), routing_key)
+        except Exception as e:
+            logging.error(f"Error al enviar el mensaje: {type(e).__name__}: {e}")
+            
+def initialize_config():
+    """ Parse env variables to find program config params
+
+    Function that search and parse program configuration parameters in the
+    program environment variables first and the in a config file. 
+    If at least one of the config parameters is not found a KeyError exception 
+    is thrown. If a parameter could not be parsed, a ValueError is thrown. 
+    If parsing succeeded, the function returns a dict with config parameters
+    """
+    config_params = {
+        "rabbitmq_host": os.getenv('RABBITMQ_HOST'),
+        "input_queue": os.getenv('INPUT_QUEUE'),
+        "output_queue_1": os.getenv('OUTPUT_QUEUE_1'),
+        "output_queue_2": os.getenv('OUTPUT_QUEUE_2'),
+        "output_exchange_filter": os.getenv('EXCHANGE_NAME'),
+        "logging_level": os.getenv('LOG_LEVEL', 'INFO'),
+        "expected_acks": int(os.getenv('EXPECTED_ACKS')),
+    }
+
+    required_keys = [
+        "rabbitmq_host",
+        "input_queue",
+        "output_queue_1",
+        "output_queue_2",
+        "output_exchange_filter",
+    ]
+
+    missing_keys = [key for key in required_keys if config_params[key] is None]
+    if missing_keys:
+        raise ValueError(f"Expected value(s) not found for: {', '.join(missing_keys)}. Aborting filter.")
+    
+    return config_params
+
+def main():
+    config_params = initialize_config()
+
+    initialize_log(config_params["logging_level"])
+
+    eof_input_queue = MessageMiddlewareQueue(config_params["rabbitmq_host"], config_params["input_queue"])
+
+    eof_output_exchange = MessageMiddlewareExchange(config_params["rabbitmq_host"], config_params["output_exchange_filter"], 
+                                        {config_params["output_queue_1"]: [str(MESSAGE_TYPE_EOF)], 
+                                        config_params["output_queue_2"]: [str(MESSAGE_TYPE_EOF)]})
+   
+    eof_service = EofServiceTime(
+        expected_acks=config_params["expected_acks"],
+        eof_in_queque=eof_input_queue,
+        eof_out_middleware=eof_output_exchange
+    )
+    eof_service.start()
+    
+if __name__ == "__main__":
+    main()
