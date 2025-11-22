1f43a22977d38a24481a2436186522d83883f19b
carolinamauro <carolina@macBookPro.local>
2025-11-22 19:02:23 -0300

    refactor: worker time

--- worker/filter/time.py
+++ worker/filter/time.py
@@ -2,7 +2,7 @@ from worker.base import Worker
 from Middleware.middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
 import logging
 from pkg.message.message import Message
-from utils.custom_logging import initialize_log, setup_process_logger
+from utils.custom_logging import initialize_log
 import os
 from pkg.message.constants import MESSAGE_TYPE_EOF, MESSAGE_TYPE_TRANSACTIONS
 from multiprocessing import Process
@@ -11,25 +11,32 @@ from utils.heartbeat import start_heartbeat_sender
 class FilterTimeNode(Worker):
     
     def __init__(self, 
-                 data_in_queue: MessageMiddlewareQueue, 
-                 data_out_exchange: MessageMiddlewareExchange, 
-                 eof_out_exchange: MessageMiddlewareExchange,
-                 host: str, eof_service_queue: str, eof_self_queue: str,
+                 data_input_queue: str, 
+                 data_output_exchange: str, 
+                 output_exchange_queues: dict,
+                 eof_output_exchange: str,
+                 eof_output_queues: dict,
+                 eof_self_queue: str,
+                 eof_service_queue: str,
+                 host: str, 
                  time_set):
+        
         self.__init_manager__()
-        self.data_input_queue = data_in_queue
-        self.data_out_exchange = data_out_exchange
-        self.eof_out_exchange = eof_out_exchange
+        self.__init_middlewares_handler__()
+        self.data_input_queue = data_input_queue
+        self.data_output_exchange = data_output_exchange
+        self.output_exchange_queues = output_exchange_queues
+        self.eof_output_exchange = eof_output_exchange
+        self.eof_output_queues = eof_output_queues
         self.host = host
         self.eof_service_queue = eof_service_queue
         self.eof_self_queue = eof_self_queue
         self.time = time_set
 
-
     def start(self):
        
         logging.info(f"Starting process")
-        p_data = Process(target=self._consume_data_queue, args=(self.data_input_queue,))
+        p_data = Process(target=self._consume_data_queue)
         
         logging.info(f"Starting EOF node process")
         p_eof = Process(target=self._consume_eof)
@@ -39,73 +46,50 @@ class FilterTimeNode(Worker):
         for p in (p_data, p_eof): p.start()
         for p in (p_data, p_eof): p.join()
         
-
-    def _consume_data_queue(self, queue: MessageMiddlewareQueue):
-        queue.start_consuming(self.__on_message__)
-
-    def _consume_eof(self): 
-        eof_service_queue = MessageMiddlewareQueue(self.host, self.eof_service_queue)
-        eof_self_queue = MessageMiddlewareQueue(self.host, self.eof_self_queue)
-    
-        def on_eof_message(message):
+    def _consume_data_queue(self):
+        data_in_queue = MessageMiddlewareQueue(self.host, self.data_input_queue)
+        data_output_exchange = MessageMiddlewareExchange(self.host, self.data_output_exchange, self.output_exchange_queues)
+        eof_output_exchange = MessageMiddlewareExchange(self.host, self.eof_output_exchange, self.eof_output_queues)
+        self.message_middlewares.extend([data_in_queue, data_output_exchange, eof_output_exchange])
+        
+        def __on_message__(message):
             try:
                 message = Message.deserialize(message)
+
                 if message.type == MESSAGE_TYPE_EOF:
-                    logging.info(f"EOF recibido en nodo | request_id: {message.request_id} | type: {message.type}")
-                    self._ensure_request(message.request_id)
-                    self.drained[message.request_id].wait()
-                    eof_service_queue.send(message.serialize())
+                    logging.info(f"action: EOF message received in data queue | request_id: {message.request_id}")
+                    eof_output_exchange.send(message.serialize(), str(message.type))
+                    return
+
+                logging.debug(f"action: message received in data queue | request_id: {message.request_id} | msg_type: {message.type}")
+                
+                self._ensure_request(message.request_id)
+                self._inc_inflight(message.request_id)
+
+                items = message.process_message()
+
+                if not items:
+                    logging.info(f"action: no more items to process | request_id: {message.request_id} | type: {message.type}")
+                    return
+
+                new_chunk = '' 
+                for item in items:
+                    item_time = item.get_time()
+                    time = item_time.hour
+                    if time > min(self.time) and time < max(self.time):
+                        new_chunk += item.serialize()
+                
+                if new_chunk:
+                    message.update_content(new_chunk)
+                    serialized = message.serialize()
+                    data_output_exchange.send(serialized, str(message.type))
+                    
             except Exception as e:
-                logging.error(f"Error al procesar el mensaje: {type(e).__name__}: {e}")
-        
-        eof_self_queue.start_consuming(on_eof_message)
+                logging.error(f"action: ERROR processing message | error: {type(e).__name__}: {e}")
+            finally:
+                self._dec_inflight(message.request_id)
 
-
-    def __on_message__(self, message):
-        try:
-            message = Message.deserialize(message)
-
-            if message.type == MESSAGE_TYPE_EOF:
-                logging.info(f"EOF recibido en Self EOF Queue | request_id: {message.request_id}")
-                self.eof_out_exchange.send(message.serialize(), str(message.type))
-                return
-
-            logging.info(f"Mensaje recibido | request_id: {message.request_id} | type: {message.type}")
-            self._ensure_request(message.request_id)
-
-            self._inc_inflight(message.request_id)
-
-            items = message.process_message()
-
-            if not items:
-                logging.info(f"No hay items en el mensaje | request_id: {message.request_id} | type: {message.type}")
-                return
-
-            new_chunk = '' 
-            for item in items:
-                item_time = item.get_time()
-                time = item_time.hour
-                if time > min(self.time) and time < max(self.time):
-                    new_chunk += item.serialize()
-            
-            if new_chunk:
-                message.update_content(new_chunk)
-                serialized = message.serialize()
-                self.data_out_exchange.send(serialized, str(message.type))
-        except Exception as e:
-            logging.error(f"Error al procesar el mensaje: {type(e).__name__}: {e}")
-
-        finally:
-            self._dec_inflight(message.request_id)
-
-    def close(self):
-        try:
-            self.data_input_queue.close()
-            self.data_out_exchange.close()
-            self.eof_out_exchange.close()
-        except Exception as e:
-            logging.error(f"Error al cerrar las conexiones: {type(e).__name__}: {e}")
-
+        data_in_queue.start_consuming(__on_message__)
 
 def initialize_config():
     """ Parse env variables to find program config params
@@ -150,18 +134,20 @@ def main():
 
     initialize_log(config_params["logging_level"])
 
-    data_input_queue = MessageMiddlewareQueue(config_params["rabbitmq_host"], config_params["input_queue"])
+    output_exchange_queues = {config_params["output_queue_1"]: [str(MESSAGE_TYPE_TRANSACTIONS), str(MESSAGE_TYPE_EOF)], 
+                            config_params["output_queue_2"]: [str(MESSAGE_TYPE_TRANSACTIONS), str(MESSAGE_TYPE_EOF)]}
+    
+    eof_output_queues = {config_params["eof_queue_1"]: [str(MESSAGE_TYPE_EOF)],
+                        config_params["eof_queue_2"]: [str(MESSAGE_TYPE_EOF)]}
 
-    data_output_exchange = MessageMiddlewareExchange(config_params["rabbitmq_host"], config_params["output_exchange_filter_time"], 
-                                        {config_params["output_queue_1"]: [str(MESSAGE_TYPE_TRANSACTIONS), str(MESSAGE_TYPE_EOF)], 
-                                        config_params["output_queue_2"]: [str(MESSAGE_TYPE_TRANSACTIONS), str(MESSAGE_TYPE_EOF)]})
-
-    eof_exchange = MessageMiddlewareExchange(config_params["rabbitmq_host"], config_params["eof_exchange_name"],
-                                        {config_params["eof_queue_1"]: [str(MESSAGE_TYPE_EOF)],
-                                         config_params["eof_queue_2"]: [str(MESSAGE_TYPE_EOF)]})
-
-
-    filter = FilterTimeNode(data_input_queue, data_output_exchange, eof_exchange, config_params["rabbitmq_host"], config_params["eof_service_queue"],  config_params["eof_self_queue"], {6, 23})
+    filter = FilterTimeNode(config_params["input_queue"], 
+                            config_params["output_exchange_filter_time"],
+                            output_exchange_queues,
+                            config_params["eof_exchange_name"],
+                            eof_output_queues,
+                            config_params["eof_self_queue"],
+                            config_params["eof_service_queue"],
+                            config_params["rabbitmq_host"], {6, 23})
     filter.start()
 
 if __name__ == "__main__":
