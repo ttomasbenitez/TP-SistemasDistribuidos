fde499a3f06bdfed3beadbe7df8ecc997d9f5b4d
Tomás <tomivenitez.14@gmail.com>
2025-11-28 21:59:24 -0300

    Send message batch and fix q2.

--- pkg/storage/state_storage/max_quantiry_profit.py
+++ pkg/storage/state_storage/max_quantiry_profit.py
@@ -22,7 +22,12 @@ class QuantityAndProfitStateStorage(StateStorage):
             if ym not in self.data_by_request[request_id]:
                 self.data_by_request[request_id][ym] = {}
                 
-            self.data_by_request[request_id][ym][item.item_id] = item
+            existing_item = self.data_by_request[request_id][ym].get(item.item_id)
+            if existing_item:
+                existing_item.quantity += item.quantity
+                existing_item.subtotal += item.subtotal
+            else:
+                self.data_by_request[request_id][ym][item.item_id] = item
             
     def _save_state_to_file(self, file_handle, request_id):
         """Guarda el estado en un archivo específico para un request_id."""
